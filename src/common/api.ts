8462d2e6a191258ed7ac32fee7f0b223d1fc2d5b
myxvisual <myxvisual@live.com>
2019-09-02 17:49:25 +0800

    Update api.ts

--- src/common/api.ts
+++ src/common/api.ts
@@ -1,4 +1,4 @@
-const apiUrl = "http://118.25.189.55:8092/class-room/rooms";
+const apiUrl = "https://test.zegonetwork.com:599/class-room/rooms";
 const headers = { "content-type": "application/json" };
 
 export interface Room {
